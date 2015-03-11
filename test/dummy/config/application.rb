5eb4e70bb6f95db30ac2c1555fc5717c78663615
Mateusz Wilczyński <majko7@interia.pl>
2015-03-11 10:18:58 +0100

    remove eager load and fix resoutces list

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -16,10 +16,6 @@ module Dummy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.eager_load_paths += %W(#{config.root}/lib)
-
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
